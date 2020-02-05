0a6064536323b1057598c9dfbbd49b3a139d7a86
Edwin Gonzalez <edwingonzalez1121@gmail.com>
2020-02-05 00:25:11 -0800

    Removed old links for resume and cover letter icons

--- client/src/Containers/Download/Download.js
+++ client/src/Containers/Download/Download.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import DownloadContain from './DownloadContain';
-import resumeIcon from '../../images/Resume.jpeg';
-import resume from '../../images/EdwinG_Resume.pdf';
-import cover from '../../images/CoverLetter.pdf';
-import clIcon from '../../images/CoverLetter.jpeg';
 
 import './download.css';
 
 const Download = () => {
     return (
         <div id='download'>
-            <DownloadContain  src={resumeIcon} title={'Resume'} buttonText={'View Resume'} href={resume}/>
-            <DownloadContain src={clIcon} title={'Cover Letter'} buttonText={'View Cover Letter'} href={cover}/>
+            <DownloadContain  src={'https://d1bx69z06fa2ip.cloudfront.net/EdwinG_Resume.jpeg/Resume.jpeg'} title={'Resume'} buttonText={'View Resume'} href={'https://d1bx69z06fa2ip.cloudfront.net/EdwinG_Resume.jpeg'}/>
+            <DownloadContain src={'https://d1bx69z06fa2ip.cloudfront.net/EdwinG_Resume.jpeg/CoverLetter.jpeg'} title={'Cover Letter'} buttonText={'View Cover Letter'} href={'https://d1bx69z06fa2ip.cloudfront.net/CoverLetter.jpeg'}/>
         </div>
     );
 }
