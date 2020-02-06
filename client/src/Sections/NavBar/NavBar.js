2b7e9c8621a000303ac4890f4dcef9ff6950195a
Edwin Gonzalez <edwingonzalez1121@gmail.com>
2020-02-05 22:06:30 -0800

    Menu button for displays smaller than 768px

--- client/src/Sections/NavBar/NavBar.js
+++ client/src/Sections/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css';
 
 //NavBar is going to be responsible for holding the section titles and when clicked on moves the user to the respective section
@@ -11,13 +11,19 @@ import './navbar.css';
 //inline the nav bar and flex box the logo and the tags 
 
 const NavBar = () => {
-    const sections = [{name: 'home', content:<img src={'https://d1bx69z06fa2ip.cloudfront.net/eg-logo.png'} alt='logo' className='logo'/>},{name: "summary", content: "Summary"}, {name: "projects",content: "Projects"}, {name: "download",content: "Download"}, {name: "contact",content: "Contact"}];
+
+    const[show, setShow] = useState(false);
+
+    const displaySections = show ? 'toggle-display': 'toggle-none';
+    const showIcon  = show ? 'menu-close': 'menu-toggle';
+    const sections = [{name: 'home', content: <img src={'https://d1bx69z06fa2ip.cloudfront.net/eg-logo.png'} alt='logo' className='logo'/>},{name: "summary", content: "Summary"}, {name: "projects",content: "Projects"}, {name: "download",content: "Download"}, {name: "contact",content: "Contact"}];
 
     return <nav className='navbar'>
-        <div className="nav-contents">
+        <div className={`nav-contents ${displaySections}`}>
             <ul>
-                {sections.map((item,i) => <li key={i}><a href={'#'+item.name}>{item.content}</a></li>)}
+                {sections.map((item,i) => <li key={i}><a href={'#'+item.name}>{item.content} </a></li>)}
             </ul>
+            <span className={showIcon} onClick={() => setShow(!show)}></span>
         </div>
         
     </nav>
